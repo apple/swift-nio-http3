5a9ed08d16fb0222ffe6d02c3ceea2a010cec1f3
Fabian Fett <fabianfett@apple.com>
2026-09-10 16:00:52 +0200

    Compute the Huffman encoded length just once (#49)

--- Tests/HTTP3Tests/TestUtil.swift
+++ Tests/HTTP3Tests/TestUtil.swift
@@ -36,3 +36,13 @@ extension HTTP3GoawayID: ExpressibleByIntegerLiteral {
         self.init(rawValue: value)
     }
 }
+
+extension ByteBuffer {
+    @discardableResult
+    mutating func writeHuffmanEncoded(bytes stringBytes: some Collection<UInt8>) -> Int {
+        self.writeHuffmanEncoded(
+            bytes: stringBytes,
+            encodedByteLength: ByteBuffer.huffmanEncodedByteLength(of: stringBytes)
+        )
+    }
+}
